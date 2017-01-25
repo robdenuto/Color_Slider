69c8d0c6e1dc6c4bed82b33030d008f939f27817
ROBERT DENUTO <rdenuto@amazon.com>
2017-01-24 19:30:43 -0500

    Initial Commit

---
+++ color_slider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  color_slider
+//
+//  Created by DeNuto, Robert on 1/24/17.
+//  Copyright © 2017 rdenuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
